2c907d039ad755c81ff770d1b945d1b85d16482e
Kyle Panuringan <kylepanuringan@gmail.com>
2022-06-19 18:53:48 +0800

    Add responsive navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [showLink, setShowLink] = React.useState(false);
+
+	return (
+		<nav>
+			<div className="nav-base">
+				<div className="nav-header">
+					<h1>E-Commerce</h1>
+					<div className="nav-buttons">
+						<div className="nav-cart-items">
+							<button className="nav-cart">
+								<p className="nav-cart-numbers">01</p>
+								<FaShoppingCart />
+							</button>
+						</div>
+						<button
+							className="nav-hamburger"
+							onClick={() => setShowLink(!showLink)}
+						>
+							<GiHamburgerMenu />
+						</button>
+					</div>
+				</div>
+				<div className={`links ${showLink && "show-links"}`}>
+					<a href="#">Home</a>
+					<a href="#">Store</a>
+					<a href="#">Contact Us</a>
+					<a href="#">About Us</a>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
